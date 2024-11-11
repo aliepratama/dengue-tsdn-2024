b7951b1b011eb322357b3b6b07ef41eaabf8e07a
aliepratama <aliepratama2@gmail.com>
2024-11-12 02:21:18 +0700

    feat: start form page

--- dengue/controllers/form.py
+++ dengue/controllers/form.py
@@ -1,12 +1,14 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, Response
 from dengue.utils.stepper import Stepper
 
 
 def get_form() -> str:
-    return render_template('form.html')
-
-
-def post_form() -> str:
-    if request.args.get('s'):
-        Stepper.command(request.args['s'])
+    print(Stepper.get_step())
     return render_template('form.html', step=Stepper.get_step())
+
+
+def post_form() -> str | Response:
+    if request.form.get('s'):
+        Stepper.command(request.form['s'])
+        return redirect(url_for('form'))
+    return render_template('form.html')
