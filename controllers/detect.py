d262b025d3e382d2a6cc81e29bfd2a7604cdc363
aliepratama <aliepratama2@gmail.com>
2024-11-11 12:02:41 +0700

    feat: add boilerplate for project

---
+++ controllers/detect.py
@@ -0,0 +1,7 @@
+from flask import render_template
+
+def get_detect():
+    return render_template('detect.html')
+
+def post_detect():
+    return render_template('detect.html')
---
+++ controllers/scan.py
@@ -0,0 +1,7 @@
+from flask import render_template
+
+def get_scan():
+    return render_template('scan.html')
+
+def post_scan():
+    return render_template('scan.html')
