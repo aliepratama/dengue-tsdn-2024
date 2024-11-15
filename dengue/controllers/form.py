2710ef221358441401d87b2b8d062dbc4ed030af
aliepratama <aliepratama2@gmail.com>
2024-11-16 02:19:28 +0700

    fix: add scaler into form

--- dengue/controllers/form.py
+++ dengue/controllers/form.py
@@ -15,6 +15,17 @@ def post_form() -> str | Response:
     is_demam = request.form.get('KDEMA') == 'Iya'
     is_uji_lab = request.form.get('ULABO') == 'Sudah'
 
+    df_normalization = pd.DataFrame({
+        'dengue.days': int(request.form.get('DDEMA')) if is_demam else 0,
+        'current_temp': float(request.form.get('SUHUN')) * 9 / 5 + 32 if is_demam else 0,
+        'dengue.wbc': float(request.form.get('JWBCS')) if is_uji_lab else 0,
+        'dengue.hemoglobin': float(request.form.get('HEMOG')) if is_uji_lab else 0,
+        'dengue._hematocri': int(request.form.get('HEMAT')) if is_uji_lab else 0,
+        'dengue.platelet': int(request.form.get('JPLAT')) if is_uji_lab else 0,
+    }, index=[0])
+    normalizer = joblib.load(os.path.join(os.getcwd(), 'dengue/models', 'normalizer.joblib'))
+    df_normalization = pd.DataFrame(normalizer(df_normalization), columns=df_normalization.columns)
+
     general_symptoms = {
         'SKPLA': 'dengue.servere_headche',
         'NYMAT': 'dengue.pain_behind_the_eyes',
@@ -32,18 +43,20 @@ def post_form() -> str | Response:
 
     if is_demam:
         df_fever = pd.DataFrame({
-            'dengue.days': int(request.form.get('DDEMA')), 
-            'current_temp': float(request.form.get('SUHUN')),
+            'dengue.days': df_normalization['dengue.days'], 
+            'current_temp': df_normalization['current_temp'],
         }, index=[0])
 
     if is_uji_lab:
         df_lab = pd.DataFrame({
-            'dengue.wbc': float(request.form.get('JWBCS')),
-            'dengue.hemoglobin': float(request.form.get('HEMOG')),
-            'dengue._hematocri': int(request.form.get('HEMAT')),
-            'dengue.platelet': int(request.form.get('JPLAT')),
+            'dengue.wbc': df_normalization['dengue.wbc'],
+            'dengue.hemoglobin': df_normalization['dengue.hemoglobin'],
+            'dengue._hematocri': df_normalization['dengue._hematocri'],
+            'dengue.platelet': df_normalization['dengue.platelet'],
         }, index=[0])
-    
+
+    print(df_normalization)
+
     if is_demam and is_uji_lab:
         model = joblib.load(os.path.join(os.getcwd(), 'dengue/models', 'all_data.joblib'))
         prediction = model.predict(pd.concat([df_fever, df_lab, df_general], axis=1))
