3d56f2d02120580c09a8cdc885c2f77b944328b8
aliepratama <aliepratama2@gmail.com>
2024-11-17 22:35:45 +0700

    fix: deployment problem

--- dengue/app.py
+++ dengue/app.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import json
+import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 from dengue.controllers import index as idx, scan as scn, form as frm, handler as hdl
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
+from pyngrok import ngrok
 
 load_dotenv()
 
@@ -18,8 +21,20 @@ db = SQLAlchemy(model_class=Base)
 
 
 def init_webhooks(base_url):
-    # Update inbound traffic via APIs to use the public-facing ngrok URL
-    pass
+    url = "https://api.short.io/links/lnk_4QHP_vWHlRVp7TBsqmoq1HBmxy"
+
+    payload = json.dumps({"allowDuplicates": False, 
+                        "domain": "share.aliepr.my.id", 
+                        "path": 'dengue-tsdn',
+                        'originalURL': base_url,
+                        })
+    headers = {
+        'accept': "application/json",
+        'content-type': "application/json",
+        'authorization': os.environ.get("SHORTIO_API_KEY")
+        }
+    response = requests.post(url, data=payload, headers=headers)
+    print(response.text)
 
 
 def create_app():
@@ -31,28 +46,20 @@ def create_app():
 
     Migrate(app,db)
 
-    # Initialize our ngrok settings into Flask
     app.config.from_mapping(
         BASE_URL="http://localhost:5000",
-        USE_NGROK=os.environ.get("USE_NGROK", "False") == "True" and os.environ.get(
-            "WERKZEUG_RUN_MAIN") != "true"
+        USE_NGROK=os.environ.get("USE_NGROK", "False") == "True"
     )
 
     if app.config["USE_NGROK"] and os.environ.get("NGROK_AUTHTOKEN"):
-        # pyngrok will only be installed, and should only ever be initialized, in a dev environment
-        from pyngrok import ngrok
 
-        # Get the dev server port (defaults to 5000 for Flask, can be overridden with `--port`
-        # when starting the server
         port = sys.argv[sys.argv.index(
             "--port") + 1] if "--port" in sys.argv else "5000"
 
-        # Open a ngrok tunnel to the dev server
         public_url = ngrok.connect(port).public_url
         print(
             f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
 
-        # Update any base URLs or webhooks to use the public ngrok URL
         app.config["BASE_URL"] = public_url
         init_webhooks(public_url)
 
